1f0d37a1be8a86b3d08b8009b2ba52f5ef5fc369
gigayulianti <yulianti2@gmail.com>
2026-05-12 11:49:33 +0700

    coomit 18

--- StackUingLinkedList.cpp
+++ StackUingLinkedList.cpp
@@ -111,4 +111,10 @@ int main()
             }
             break;
         case 3:
-            
+            if (!stack.isEmpty())
+            {
+                stack.peek(); //get the value of the top element
+            }
+            else
+            {
+                
