d89f95ec7e7bfc4d37dead30166801ccc251f340
gigayulianti <yulianti2@gmail.com>
2026-05-12 11:47:32 +0700

    coomit 15

--- StackUingLinkedList.cpp
+++ StackUingLinkedList.cpp
@@ -93,4 +93,10 @@ int main()
         cout << "Enter your choice: ";
         cin >> choice;
 
-        
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value); //push the entered value onto the stack
+            
