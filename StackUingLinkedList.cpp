2fdaefca22e81a15071eb3774c6f165949157199
gigayulianti <yulianti2@gmail.com>
2026-05-12 11:48:46 +0700

    coomit 17

--- StackUingLinkedList.cpp
+++ StackUingLinkedList.cpp
@@ -105,4 +105,10 @@ int main()
             {
                 stack.pop(); //pop the top element from the stack
             }
+            else 
+            {
+                cout << "Stack is empty. Cannot pop." << endl;
+            }
+            break;
+        case 3:
             
